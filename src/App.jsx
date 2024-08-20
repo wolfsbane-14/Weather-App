045ca04d3fff00abd6304854de6209b112217e68
wolfsbane-14 <jatinsinghfourteen@gmail.com>
2024-08-20 22:12:38 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+
+import "./App.css";
+
+const api = {
+  key: "db7e01de5bfcd0ea085f5dafb212f49f",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  const getClassName = () => {
+    if (typeof weather.main !== "undefined") {
+      const weatherMain = weather.weather[0].main.toLowerCase();
+      console.log(`OUTPUTTT ISSSS app ${weatherMain}`)
+      return `app ${weatherMain}`;
+    }
+    console.log("OUTPUT ISS app")
+    return 'app';
+  }
+
+  return (
+    <div className={getClassName()}>
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {" "}
+                {weather.name},{weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°C</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ("")}
+      </main>
+    </div>
+  );
+}
+
+export default App;
